2ab949381d178568a65dd4d89ed9255d6c24b800
Jim O'Brien <jim@jimmed.net>
2017-03-20 01:09:19 +0000

    [master] Test tweaks

--- src/components/MessageEditor/reducer.test.js
+++ src/components/MessageEditor/reducer.test.js
@@ -1,55 +1,36 @@
-const {createStore} = require('redux')
 const {createTree} = require('redux-tree')
+const {describeReducer} = require('../../utils/tests')
 const actions = require('./actions')
-const reducerBranch = require('./reducer')
+const reducer = require('./reducer')
 
-const reducer = createTree({text: reducerBranch})
+describeReducer(
+  'MessageEditor reducer',
+  createTree({text: reducer}),
+  ({getState, dispatch}) => {
+    test('is a function', () => {
+      expect(typeof reducer).toBe('function')
+    })
 
-describe('MessageEditor reducer', () => {
-  let store
-  let state
+    describe('Initial state', () => {
+      test('should have empty text', () => {
+        expect(getState().text).toBeFalsy()
+      })
+    })
 
-  const updateState = () => {
-    state = store.getState().text
+    describe('On change', () => {
+      beforeEach(() => dispatch(actions.changeText('Edited message!')))
+
+      test('should change', () => {
+        expect(getState().text).toBe('Edited message!')
+      })
+    })
+
+    describe('After sending message', () => {
+      beforeEach(() => dispatch(actions.sendMessage('Sent message!')))
+
+      test('should reset', () => {
+        expect(getState().text).toBe('')
+      })
+    })
   }
-  const dispatchAndUpdate = action => {
-    store.dispatch(action)
-    updateState()
-  }
-
-  beforeEach(() => {
-    store = createStore(reducer)
-    updateState()
-  })
-
-  afterEach(() => {
-    store = null
-    state = null
-  })
-
-  test('is a function', () => {
-    expect(typeof reducer).toBe('function')
-  })
-
-  describe('Initial state', () => {
-    test('should have empty text', () => {
-      expect(state).toBeFalsy()
-    })
-  })
-
-  describe('On change', () => {
-    beforeEach(() => dispatchAndUpdate(actions.changeText('Edited message!')))
-
-    test('should change', () => {
-      expect(state).toBe('Edited message!')
-    })
-  })
-
-  describe('After sending message', () => {
-    beforeEach(() => dispatchAndUpdate(actions.sendMessage('Sent message!')))
-
-    test('should reset', () => {
-      expect(state).toBe('')
-    })
-  })
-})
+)
---
+++ src/utils/tests.js
@@ -0,0 +1,33 @@
+const {createStore} = require('redux')
+
+function describeReducer(name, reducer, tests) {
+  let store
+  let state
+
+  const updateState = () => {
+    state = store.getState()
+  }
+
+  const dispatch = action => {
+    store.dispatch(action)
+    updateState()
+  }
+
+  const getState = () => state
+
+  beforeEach(() => {
+    store = createStore(reducer)
+    updateState()
+  })
+
+  afterEach(() => {
+    store = null
+    state = null
+  })
+
+  tests({dispatch, getState})
+}
+
+module.exports = {
+  describeReducer
+}
