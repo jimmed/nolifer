7ce9730df3fcf4fbaff7ed52e1178bae87cc642d
Jim O'Brien <jim@jimmed.net>
2017-04-19 18:21:13 +0100

    Add Timestamp component for live-updating timestamps

--- src/components/MessageHistory/index.js
+++ src/components/MessageHistory/index.js
@@ -1,86 +1,74 @@
-const React = require('react')
-const distanceInWordsStrict = require('date-fns/distance_in_words_strict')
-const parseDate = require('date-fns/parse')
-const style = require('./style.css')
+const React = require('react');
+const Timestamp = require('../Timestamp');
+const style = require('./style.css');
 
 class MessageHistory extends React.PureComponent {
   constructor(props) {
-    super(props)
-    this.captureLastMessage = this.captureLastMessage.bind(this)
+    super(props);
+    this.captureLastMessage = this.captureLastMessage.bind(this);
   }
 
   captureLastMessage(ref) {
-    this._lastMessage = ref
+    this._lastMessage = ref;
   }
 
   scrollIntoView() {
     if (this._lastMessage) {
-      this._lastMessage.scrollIntoView({behavior: 'smooth'})
+      this._lastMessage.scrollIntoView({ behavior: 'smooth' });
     }
   }
 
   componentDidMount() {
-    this.scrollIntoView()
+    this.scrollIntoView();
   }
 
   componentDidUpdate() {
-    this.scrollIntoView()
+    this.scrollIntoView();
   }
 
   render() {
-    const {messages} = this.props
+    const { messages } = this.props;
     return (
       <div className={style.container}>
-        {messages.length ?
-          messages
-            .slice(-1000)
-            .map(message => (
-              <MessageHistory.Message
-                key={message.id}
-                {...message}
-              />
-            ))
-            .concat([
-              <div
-                className={style.afterLastMessage}
-                key="afterLastMessage"
-                ref={this.captureLastMessage}
-              />
-            ]) :
-          (
-            <div className={style.noMessages}>
-              <p>{'It\'s a bit quiet in here!'}</p>
-            </div>
-          )
-        }
+        {messages.length
+          ? messages
+              .slice(-1000)
+              .map(message => (
+                <MessageHistory.Message key={message.id} {...message} />
+              ))
+              .concat([
+                <div
+                  className={style.afterLastMessage}
+                  key="afterLastMessage"
+                  ref={this.captureLastMessage}
+                />
+              ])
+          : <div className={style.noMessages}>
+              <p>{"It's a bit quiet in here!"}</p>
+            </div>}
       </div>
-    )
+    );
   }
 }
 
-MessageHistory.Message = ({sender, content, timestamp}) => (
+MessageHistory.Message = ({ sender, content, timestamp }) => (
   <div className={style.message}>
     <div className={style.sender}>{sender}</div>
     <div className={style.content}>{content}</div>
-    <div className={style.timestamp} title={parseDate(timestamp).toLocaleString()}>
-      {distanceInWordsStrict(Date.now(), timestamp, {
-        addSuffix: true,
-        includeSeconds: true
-      })}
-    </div>
+    <Timestamp className={style.timestamp} timestamp={timestamp} />
   </div>
-)
+);
 
 MessageHistory.Message.propTypes = {
   sender: React.PropTypes.string.isRequired,
   content: React.PropTypes.string.isRequired,
   timestamp: React.PropTypes.number.isRequired
-}
+};
 
 MessageHistory.propTypes = {
   messages: React.PropTypes.arrayOf(
     React.PropTypes.shape(MessageHistory.Message.propTypes)
   ).isRequired
-}
+};
 
-module.exports = MessageHistory
+module.exports = MessageHistory;
---
+++ src/components/Timestamp/index.js
@@ -0,0 +1,74 @@
+const React = require('react');
+const distanceInWordsStrict = require('date-fns/distance_in_words_strict');
+const parseDate = require('date-fns/parse');
+
+const subscribers = {};
+let lastSubscriberId = 0;
+let tickInterval;
+
+const getSubscribers = () => Object.keys(subscribers).map(i => subscribers[i]);
+
+const startTicking = () =>
+  tickInterval = setInterval(() => getSubscribers().map(fn => fn()), 1000);
+const stopTicking = () => clearInterval(tickInterval);
+
+const subscribeToTick = updateComponent => {
+  const existingSubscribers = getSubscribers();
+  const subscriberId = ++lastSubscriberId;
+  subscribers[subscriberId] = updateComponent;
+  if (!existingSubscribers.length) {
+    startTicking();
+  }
+  return function unsubscribeFromTick() {
+    delete subscribers[subscriberId];
+    if (!getSubscribers().length) {
+      stopTicking();
+    }
+  };
+};
+
+const defaultFormatTitle = timestamp => parseDate(timestamp).toLocaleString();
+const defaultFormatText = timestamp =>
+  distanceInWordsStrict(Date.now(), timestamp, {
+    addSuffix: true,
+    includeSeconds: true
+  });
+
+class Timestamp extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = this.stateFromProps(props);
+    this.update = this.update.bind(this);
+  }
+
+  componentDidMount() {
+    this._unsubscribeFromTick = subscribeToTick(this.update);
+  }
+
+  componentWillUnmount() {
+    this._unsubscribeFromTick();
+  }
+
+  update() {
+    this.setState(() => this.stateFromProps(this.props));
+  }
+
+  stateFromProps({
+    timestamp,
+    formatTitle = defaultFormatTitle,
+    formatText = defaultFormatText
+  }) {
+    return {
+      title: formatTitle(timestamp),
+      text: formatText(timestamp)
+    };
+  }
+
+  render() {
+    const { title, text } = this.state;
+    const { className } = this.props;
+    return <span className={className} title={title}>{text}</span>;
+  }
+}
+
+module.exports = Timestamp;
